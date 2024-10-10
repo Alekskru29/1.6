d01b019ba0f3bff9db6bda381ee3748a4a91f1da
Aleksandr Krutov <aleksandrkrutov1992@gmail.com>
2024-10-11 01:18:20 +0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Практическое задание по блоку 1.6 GitHubPage: https://alekskru29.github.io/1.6/
--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import '../scss/style.scss'
-import '../blocks/popup/popup.js'
-import '../blocks/main/btn.js'
-import '../blocks/main/brands.js'
